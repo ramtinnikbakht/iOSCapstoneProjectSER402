e311a7c439e6fa661b63348d126ae6d6f080bf0a
cosorio11 <cosorio@asu.edu>
2016-03-03 14:31:57 -0700

    updating use of Storyboard, and project files

---
+++ CapstoneProjectSER402/CapstoneProjectSER402/ChangeTicket_Table_Template.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeTicket_Table_Template.swift
+//  CapstoneProjectSER402
+//
+//  Created by Cuahuc on 3/3/16.
+//  Copyright © 2016 Ramtin Nikbakht. All rights reserved.
+//
+
+import Foundation
