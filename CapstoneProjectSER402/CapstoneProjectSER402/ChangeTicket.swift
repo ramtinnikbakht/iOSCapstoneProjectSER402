ee50ab35fef3ef4717ff28bba17cb767640f688f
cosorio11 <cosorio@asu.edu>
2016-03-02 23:28:43 -0700

    updating changeTickets, and User objects

--- CapstoneProjectSER402/CapstoneProjectSER402/ChangeTicket.swift
+++ CapstoneProjectSER402/CapstoneProjectSER402/ChangeTicket.swift
@@ -5,12 +5,160 @@
 //  Created by Nick Krogstad on 2/17/16.
 //  Copyright © 2016 Ramtin Nikbakht. All rights reserved.
 //
+//  Modified by Cuahuctemoc Osorio on 3/2/16
+
 
 import Foundation
 
-struct ChangeTicket {
-    let id : String?
-    let priority : Int
+class ChangeTicket
+{
     var icon: UIImage?
     var isWatched: Bool
+    let number: String
+    var approver: String
+    var plannedStart: String
+    var plannedEnd: String
+    var actualStart: String
+    var actualEnd: String
+    var requestedByGroup: String
+    var requestedByGroupBusinessArea: String
+    var requestedByGroupBusinessUnit: String
+    var requestedByGroupSubBusinessUnit: String
+    var causeCompleteServiceAppOutage: String
+    var risk: String
+    var type: String
+    var impactScore: String
+    var shortDescription: String
+    var changeReason: String
+    var closureCode: String
+    var ImpactedEnviroment: String
+    var SecondaryClosureCode:String
+    var PartofRelease: String
+    var BusinessApplication: String
+    var BusinessApplicationCriticalityTier: String
+    
+    init(icon: UIImage, isWatched: Bool, number: String, approver: String, plannedStart: String, plannedEnd: String, actualStart: String, actualEnd: String, requestedByGroup: String, requestedByGroupBusinessArea:String, requestedByGroupBusinessUnit: String, requestedByGroupSubBusinessUnit: String, causeCompleteServiceAppOutage: String, risk: String, type:String, impactScore:String, shortDescription:String, changeReason: String, closureCode: String, ImpactedEnviroment: String, SecondaryClosureCode: String, PartofRelease: String, BusinessApplication: String, BusinessApplicationCriticalityTier: String)
+    {
+        self.icon = icon
+        self.isWatched = isWatched
+        self.number = number
+        self.approver = approver
+        self.plannedStart = plannedStart
+        self.plannedEnd = plannedEnd
+        self.actualStart = actualStart
+        self.actualEnd = actualEnd
+        self.requestedByGroup = requestedByGroup
+        self.requestedByGroupBusinessArea = requestedByGroupBusinessArea
+        self.requestedByGroupBusinessUnit = requestedByGroupBusinessUnit
+        self.requestedByGroupSubBusinessUnit = requestedByGroupSubBusinessUnit
+        self.causeCompleteServiceAppOutage = causeCompleteServiceAppOutage
+        self.risk = risk
+        self.type = type
+        self.impactScore = impactScore
+        self.shortDescription = shortDescription
+        self.changeReason = changeReason
+        self.closureCode = closureCode
+        self.ImpactedEnviroment = ImpactedEnviroment
+        self.SecondaryClosureCode = SecondaryClosureCode
+        self.PartofRelease = PartofRelease
+        self.BusinessApplication = BusinessApplication
+        self.BusinessApplicationCriticalityTier = BusinessApplicationCriticalityTier
+    }
+    
+    func getIcon() -> UIImage
+    {
+        return self.icon!
+    }
+    func getIsWatched() -> Bool
+    {
+        return self.isWatched
+    }
+    func getNumber() -> String
+    {
+        return self.number
+    }
+    func getApprover() -> String
+    {
+        return self.approver
+    }
+    func getPlannedStart() -> String
+    {
+        return self.plannedStart
+    }
+    func getPlannedEnd() -> String
+    {
+        return self.plannedEnd
+    }
+    func getActualStart() -> String
+    {
+        return self.actualStart
+    }
+    func getActualEnd()-> String
+    {
+        return self.actualEnd
+    }
+    func getRequestedByGroup() -> String
+    {
+        return self.requestedByGroup
+    }
+    func getRequestedByGroupBusinessArea() -> String
+    {
+        return self.requestedByGroupBusinessArea
+    }
+    func getRequestedByGroupBusinessUnit() -> String
+    {
+        return self.requestedByGroupBusinessUnit
+    }
+    func getRequestedByGroupSubBusinessUnit() -> String
+    {
+        return self.requestedByGroupSubBusinessUnit
+    }
+    func getCauseCompleteServiceAppOutage() -> String
+    {
+        return self.causeCompleteServiceAppOutage
+    }
+    func getRisk() -> String
+    {
+        return self.risk
+    }
+    func getType() -> String
+    {
+        return self.type
+    }
+    func getImpactScore() -> String
+    {
+        return self.impactScore
+    }
+    func getShortDescription() -> String
+    {
+        return self.shortDescription
+    }
+    func getChangeReason() -> String
+    {
+        return self.changeReason
+    }
+    func getClosureCode() -> String
+    {
+        return self.closureCode
+    }
+    func getImpactedEnviroment() -> String
+    {
+        return self.ImpactedEnviroment
+    }
+    func getSecondaryClosureCode() -> String
+    {
+        return self.SecondaryClosureCode
+    }
+    func getPartofRelease() -> String
+    {
+        return self.PartofRelease
+    }
+    func getBusinessApplication() -> String
+    {
+        return self.BusinessApplication
+    }
+    func getBusinessApplicationCriticalityTier() -> String
+    {
+        return self.BusinessApplicationCriticalityTier
+    }
 }
--- CapstoneProjectSER402/CapstoneProjectSER402/User.swift
+++ CapstoneProjectSER402/CapstoneProjectSER402/User.swift
@@ -5,18 +5,21 @@
 //  Created by Ramtin Nikbakht on 2/25/16.
 //  Copyright © 2016 Ramtin Nikbakht. All rights reserved.
 //
+//  Modified by Cuahuctemoc Osorio on 3/2/16
+
 
 import Foundation
 
-class User {
-    var usersID: Int
+class User
+{
+    var usersID: String
     var usersName: String
     var usersProfilePic: UIImage
     var usersBusinessUnit: String
     var usersAppList = [String]()
     var usersWatchList = [String]()
     
-    init(usersID: Int, usersName: String, usersProfilePic: UIImage, usersBusinessUnit: String, usersAppList: [String], usersWatchList: [String]) {
+    init(usersID: String, usersName: String, usersProfilePic: UIImage, usersBusinessUnit: String, usersAppList: [String], usersWatchList: [String]) {
         
         self.usersID = usersID
         self.usersName = usersName
@@ -25,5 +28,55 @@ class User {
         self.usersAppList = usersAppList
         self.usersWatchList = usersWatchList
         
+    }
+    //Getters
+    func getUsersID() -> String
+    {
+        return self.usersID
+    }
+    func getUsersNames() -> String
+    {
+        return self.usersName
+    }
+    func getUsersProfilePic() -> UIImage
+    {
+        return self.usersProfilePic
+    }
+    func getUsersBusinessUnit() -> String
+    {
+        return self.usersBusinessUnit
+    }
+    func getUsersID() -> [String]
+    {
+        return self.usersAppList
+    }
+    func getUsersNames() -> [String]
+    {
+        return self.usersWatchList
+    }
+    //Setters
+    func setUsersID(usersID: String)
+    {
+        self.usersID = usersID
+    }
+    func setUsersNames(usersName: String)
+    {
+        self.usersName = usersName
+    }
+    func setUsersProfilePic(usersProfilePic: UIImage)
+    {
+        self.usersProfilePic = usersProfilePic
+    }
+    func setUsersBusinessUnit(usersBusinessUnit: String)
+    {
+        self.usersBusinessUnit = usersBusinessUnit
+    }
+    func setUsersID(usersAppList: [String])
+    {
+        self.usersAppList = usersAppList
+    }
+    func setUsersNames(usersWatchList: [String])
+    {
+        self.usersWatchList = usersWatchList
     }
 }
