d17f908166950fe4185d700a0b630790c0553ede
cosorio11 <cosorio@asu.edu>
2016-03-24 14:49:51 -0700

    fixing loadSampleTickets

--- CapstoneProjectSER402/CapstoneProjectSER402/ChangeTicketTableViewController.swift
+++ CapstoneProjectSER402/CapstoneProjectSER402/ChangeTicketTableViewController.swift
@@ -50,36 +50,26 @@ class ChangeTicketTableViewController: UITableViewController
             
         }
     }
-    /*
-    func loadSampleTickets() {
-        let obj2 = ChangeTicket(number: "CHG00314757", approver: "", plannedStart: "2016-02-11 02:00:00", plannedEnd: "2016-02-11 08:00:00", actualStart: "", actualEnd: "", requestedByGroup: "ServiceNow_DEV", requestedByGroupBusinessArea: "Infrastructure Services", requestedByGroupBusinessUnit: "IS – Infrastructure Services", requestedByGroupSubBusinessUnit: "IS - Production Process", causeCompleteServiceAppOutage: "No", risk: "8", type: "Normal", impactScore: "", shortDescription: "Servicenow 02/10 Release", changeReason: "New Capability", closureCode: "", ImpactedEnviroment: "Production", SecondaryClosureCode: "", PartofRelease: "", BusinessApplication: "ServiceNow Enterprise Edition", BusinessApplicationCriticalityTier: "Tier 2")
-        let obj3 = ChangeTicket(number: "CHG00318797", approver: "", plannedStart: "2016-02-11 02:00:00", plannedEnd: "2016-02-11 08:00:00", actualStart: "", actualEnd: "", requestedByGroup: "ServiceNow_DEV", requestedByGroupBusinessArea: "Infrastructure Services", requestedByGroupBusinessUnit: "IS – Infrastructure Services", requestedByGroupSubBusinessUnit: "IS - Production Process", causeCompleteServiceAppOutage: "No", risk: "2", type: "Normal", impactScore: "", shortDescription: "Servicenow 02/10 Release", changeReason: "New Capability", closureCode: "", ImpactedEnviroment: "Production", SecondaryClosureCode: "", PartofRelease: "", BusinessApplication: "ServiceNow Enterprise Edition", BusinessApplicationCriticalityTier: "Tier 2")
-        
-        let ticket1 = ChangeTicket_Table_Template(id: obj1.getNumber(), priority: obj1.getRisk(), icon: eyeIcon!, isWatched: false)
-        let ticket2 = ChangeTicket_Table_Template(id: obj2.getNumber(), priority: obj2.getRisk(), icon: eyeIcon!, isWatched: false)
-        let ticket3 = ChangeTicket_Table_Template(id: obj3.getNumber(), priority: obj3.getRisk(), icon: eyeIcon!, isWatched: false)
-        let ticket4 = ChangeTicket_Table_Template(id: "CHG-004", priority: "5", icon: eyeIcon!, isWatched: false)
     
-        //let ticket2 = ChangeTicket()
-        let ticket1 = ChangeTicket(id: "CHG-001", priority: 8, startDate: "01/16", icon: eyeIcon, isWatched: false)
-        let ticket2 = ChangeTicket(id: "CHG-002", priority: 4, startDate: "02/16", icon: eyeIcon, isWatched: false)
-        let ticket3 = ChangeTicket(id: "CHG-003", priority: 1, startDate: "03/16", icon: eyeIcon, isWatched: false)
-        let ticket4 = ChangeTicket(id: "CHG-004", priority: 5, startDate: "04/16", icon: eyeIcon, isWatched: false)
+    func loadSampleTickets()
+    {
+        let eyeIcon = UIImage(named: "eye_unclicked.png")
+        let obj1 = ChangeTicket(number: "CHG00313717", approver: "", plannedStart: "2016-02-11 03:30:00", plannedEnd: "2016-02-11 08:00:00", actualStart: "", actualEnd: "", requestedByGroup: "ServiceNow_DEV", requestedByGroupBusinessArea: "Infrastructure Services", requestedByGroupBusinessUnit: "IS – Infrastructure Services", requestedByGroupSubBusinessUnit: "IS - Production Process", causeCompleteServiceAppOutage: "No", risk: "4", type: "Normal", impactScore: "", shortDescription: "Servicenow 02/10 Release", changeReason: "New Capability", closureCode: "", ImpactedEnviroment: "Production", SecondaryClosureCode: "", PartofRelease: "", BusinessApplication: "ServiceNow Enterprise Edition", BusinessApplicationCriticalityTier: "Tier 2")
+        let obj2 = ChangeTicket(number: "CHG00314757", approver: "", plannedStart: "2016-04-25 15:05:00", plannedEnd: "2016-02-11 08:00:00", actualStart: "", actualEnd: "", requestedByGroup: "ServiceNow_DEV", requestedByGroupBusinessArea: "Infrastructure Services", requestedByGroupBusinessUnit: "IS – Infrastructure Services", requestedByGroupSubBusinessUnit: "IS - Production Process", causeCompleteServiceAppOutage: "No", risk: "8", type: "Normal", impactScore: "", shortDescription: "Servicenow 02/10 Release", changeReason: "New Capability", closureCode: "", ImpactedEnviroment: "Production", SecondaryClosureCode: "", PartofRelease: "", BusinessApplication: "ServiceNow Enterprise Edition", BusinessApplicationCriticalityTier: "Tier 2")
+        let obj3 = ChangeTicket(number: "CHG00318797", approver: "", plannedStart: "2016-06-18 06:15:00", plannedEnd: "2016-02-11 08:00:00", actualStart: "", actualEnd: "", requestedByGroup: "ServiceNow_DEV", requestedByGroupBusinessArea: "Infrastructure Services", requestedByGroupBusinessUnit: "IS – Infrastructure Services", requestedByGroupSubBusinessUnit: "IS - Production Process", causeCompleteServiceAppOutage: "No", risk: "2", type: "Normal", impactScore: "", shortDescription: "Servicenow 02/10 Release", changeReason: "New Capability", closureCode: "", ImpactedEnviroment: "Production", SecondaryClosureCode: "", PartofRelease: "", BusinessApplication: "ServiceNow Enterprise Edition", BusinessApplicationCriticalityTier: "Tier 2")
+        
+        let ticket1 = ChangeTicket_Table_Template(id: obj1.getNumber(), priority: obj1.getRisk(),startDate: obj1.getPlannedStart(), icon: eyeIcon!, isWatched: false)
+        let ticket2 = ChangeTicket_Table_Template(id: obj2.getNumber(), priority: obj2.getRisk(),startDate: obj2.getPlannedStart(), icon: eyeIcon!, isWatched: false)
+        let ticket3 = ChangeTicket_Table_Template(id: obj3.getNumber(), priority: obj3.getRisk(),startDate: obj3.getPlannedStart(), icon: eyeIcon!, isWatched: false)
+        let ticket4 = ChangeTicket_Table_Template(id: "CHG-004", priority: "5",startDate: "2016-011-5 18:15:00", icon: eyeIcon!, isWatched: false)
+    
      
         changeTickets += [ticket1, ticket2, ticket3, ticket4]
-     let eyeIcon = UIImage(named: "eye_unclicked.png")
-     let ticket1 = ChangeTicket(id: "CHG-001", priority: 8, startDate: "01/16", icon: eyeIcon, isWatched: false)
-     let ticket2 = ChangeTicket(id: "CHG-002", priority: 4, startDate: "02/16", icon: eyeIcon, isWatched: false)
-     let ticket3 = ChangeTicket(id: "CHG-003", priority: 1, startDate: "03/16", icon: eyeIcon, isWatched: false)
-     let ticket4 = ChangeTicket(id: "CHG-004", priority: 5, startDate: "04/16", icon: eyeIcon, isWatched: false)
-     
-     changeTickets += [ticket1, ticket2, ticket3, ticket4]
-     tickets.addChangeTickets(ticket1)
-     tickets.addChangeTickets(ticket2)
-     tickets.addChangeTickets(ticket3)
-     tickets.addChangeTickets(ticket4)
-        //changeTickets += [ticket1]
-    }*/
+        tickets.addChangeTickets(ticket1)
+        tickets.addChangeTickets(ticket2)
+        tickets.addChangeTickets(ticket3)
+        tickets.addChangeTickets(ticket4)
+    }
  
     
     override func didReceiveMemoryWarning() {
