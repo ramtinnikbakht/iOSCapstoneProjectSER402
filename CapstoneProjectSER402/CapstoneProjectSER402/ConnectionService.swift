8d6e023e2e2003b4727534428f3000ad080962df
cosorio11 <cosorio@asu.edu>
2016-04-04 19:26:25 -0700

    fixing 2 small errors

--- CapstoneProjectSER402/CapstoneProjectSER402/ConnectionService.swift
+++ CapstoneProjectSER402/CapstoneProjectSER402/ConnectionService.swift
@@ -10,8 +10,8 @@ import Foundation
 
 class ConnectionService : NSObject, NSURLSessionDelegate {
     
-    static let sharedInstance = ConnectionService
-    private init(){}
+    static let sharedInstance = ConnectionService()
+    //private init(){}
     
     let userName = "sys_ws_asu_poly"
     let passWord = "ASU_Test#1"
