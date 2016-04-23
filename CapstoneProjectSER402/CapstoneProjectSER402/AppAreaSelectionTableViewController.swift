c754bd3378b79b04e58c0e0a1f4aa800300505f7
Nick Krogstad <nickkrogstad@gmail.com>
2016-04-23 11:10:48 -0700

    Fixed Table checkmark inconsistencies

--- CapstoneProjectSER402/CapstoneProjectSER402/AppAreaSelectionTableViewController.swift
+++ CapstoneProjectSER402/CapstoneProjectSER402/AppAreaSelectionTableViewController.swift
@@ -27,21 +27,11 @@ class AppAreaSelectionTableViewController: UITableViewController {
         ConnectionService.sharedInstance.getBusinessArea()
         busArea = ConnectionService.sharedInstance.areaList
         
-        var i=0
-        for _ in busArea
+        for area in busArea
         {
-            appsAreaArray.append(busArea[i].getName())
-            i += 1
+            appsAreaArray.append(area.getName())
         }
-        
-        //print(appsAreaArray)
-        //areas = ConnectionService.sharedInstance.getBusiness(appUnit: "311ab55b95b38980ce51a15d3638639c")
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        //print(areas)
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,59 +62,38 @@ class AppAreaSelectionTableViewController: UITableViewController {
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
             
         let cell = tableView.dequeueReusableCellWithIdentifier("apparea") as! AppAreaTableViewCell
         cell.appAreaTitleLabel.text = appsAreaArray[indexPath.row]
-        
+        if (selectedAppAreas.contains(appsAreaArray[indexPath.row])) {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.tintColor = UIColor(red: 0/255.0, green: 64/255.0, blue: 128/255.0, alpha: 1.0)
+            cell.accessoryType = .None
+        }
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
     {
         let cell = tableView.cellForRowAtIndexPath(indexPath) as! AppAreaTableViewCell!
+        let selectedArea = cell.appAreaTitleLabel.text!
         
-        if selectedAppAreas.contains(cell.appAreaTitleLabel.text!)
-        {
-            
-            cell.accessoryType = UITableViewCellAccessoryType.None
+        if (selectedAppAreas.contains(selectedArea)) {
             selectedAppAreas = selectedAppAreas.filter {$0 != cell.appAreaTitleLabel.text!}
-            print("worked")
-            print(selectedAppAreas)
-        }
-        else
-        {
-            cell.tintColor = UIColor(red: 0/255.0,
-                                     green: 64/255.0,
-                                     blue: 128/255.0,
-                                     alpha: 1.0)
+            cell.accessoryType = .None
+        } else {
+            cell.tintColor = UIColor(red: 0/255.0, green: 64/255.0, blue: 128/255.0, alpha: 1.0)
             cell.accessoryType = UITableViewCellAccessoryType.Checkmark
-            
-            selectedAppAreas += [cell.appAreaTitleLabel.text!]
-            
-            
-            //iterate of both selectedAppAreas & businessAreas
-            //to have the elements in selectedapparea be in selectedareas as a businessarea type
-
-            //print(selectedAppAreas)
-            //var l = 0
-            //for _ in selectedAreas
-            //{
-            //    print(selectedAreas[l].getName())
-            //    l += 1
-            //}
-            print("space between calls")
-            
+            selectedAppAreas += [selectedArea]
         }
-        
-        
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
+        selectedAreas.removeAll()
         let destViewController : AppSelectionTableViewController = segue.destinationViewController as! AppSelectionTableViewController
         destViewController.usertype = usertype
-        
         
         for(var j = 0; j<selectedAppAreas.count;j++)
         {
