2b398dc426bf0fd4e29afee718418adda53df74d
Ramtin Nikbakht <ramtinnikbakht@yahoo.com>
2016-04-24 14:49:32 -0700

    Add picker data and functionality for the ChangeUserTypeViewController

--- CapstoneProjectSER402/CapstoneProjectSER402/ChangeUserTypeViewController.swift
+++ CapstoneProjectSER402/CapstoneProjectSER402/ChangeUserTypeViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class ChangeUserTypeViewController: UIViewController {
+class ChangeUserTypeViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var currentUserTypeLabel: UILabel!
     @IBOutlet weak var newUserTypePicker: UIPickerView!
-    
+    var userTypesPickerSource = ["ITSM", "Business/Leadership", "App Owner"]
+    var currentUserType: String = ""
+    var newSelectedUserType: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        newSelectedUserType = userTypesPickerSource[newUserTypePicker.selectedRowInComponent(0)]
+        self.newUserTypePicker.dataSource = self
+        self.newUserTypePicker.delegate = self
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return userTypesPickerSource.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return userTypesPickerSource[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (row == 0) {
+            newSelectedUserType = userTypesPickerSource[0]
+        }
+        else if (row == 1) {
+            newSelectedUserType = userTypesPickerSource[1]
+        }
+        else if (row == 2) {
+            newSelectedUserType = userTypesPickerSource[2]
+        }
+        else {
+            print("No new user type was selected")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +56,5 @@ class ChangeUserTypeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
